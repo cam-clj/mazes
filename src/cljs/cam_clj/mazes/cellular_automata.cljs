1ad88228e57591142820980e5a576f767ce0abe3
Ray Miller <ray@1729.org.uk>
2014-04-21 17:13:19 +0100

    Canvas with OM wrapper.

--- src/cljs/cam_clj/mazes/cellular_automata.cljs
+++ src/cljs/cam_clj/mazes/cellular_automata.cljs
@@ -1,27 +1,20 @@
 (ns cam-clj.mazes.cellular-automata
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [monet.canvas :as canvas]
-            [cljs.core.async :refer [<! timeout]]))
+  (:require [monet.canvas :as canvas]))
 
 (enable-console-print!)
 
-(def nrows 30)
-(def ncols 30)
-(def cell-size 8)
-(def margin 4)
-
 (defn init-grid
-  []
-  (into #{} (for [r (range nrows) c (range ncols) :when (< (rand) 0.5)] [r c])))
+  [nrows ncols]
+  (into #{} (for [r (range nrows) c (range ncols) :when (< (rand) 0.3)] [r c])))
 
 (defn add-delta
-  [[x y] [delta-x delta-y]]
+  [nrows ncols [x y] [delta-x delta-y]]
   [(mod (+ x delta-x) ncols)
    (mod (+ y delta-y) nrows)])
 
 (defn neighbours
-  [cell]
-  (map (partial add-delta cell) [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [-1 1] [1 0] [1 1]]))
+  [cell nrows ncols]
+  (map (partial add-delta nrows ncols cell) [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [-1 1] [1 0] [1 1]]))
 
 (defn born?
   [num-neighbours]
@@ -32,36 +25,36 @@
   (<= 1 num-neighbours 4))
 
 (defn update-grid
-  [living?]
-  (reduce (fn [new-grid cell]
-            (let [num-neighbours (count (filter living? (neighbours cell)))]
-              (if (or (and (living? cell) (survives? num-neighbours))
-                      (and (not (living? cell)) (born? num-neighbours)))
-                (conj new-grid cell)
-                new-grid)))
-          #{}
-          (for [r (range nrows) c (range ncols)] [r c])))
+  [nrows ncols]
+  (fn
+    [living?]
+    (reduce (fn [new-grid cell]
+              (let [num-neighbours (count (filter living? (neighbours cell nrows ncols)))]
+                (if (or (and (living? cell) (survives? num-neighbours))
+                        (and (not (living? cell)) (born? num-neighbours)))
+                  (conj new-grid cell)
+                  new-grid)))
+            #{}
+            (for [r (range nrows) c (range ncols)] [r c]))))
 
 (defn render-grid
-  [ctx cells]
-  (canvas/fill-style ctx "#000000")
-  (doseq [[row col] cells]
-    (canvas/fill-rect ctx {:x (+ margin (* col cell-size))
-                           :y (+ margin (* row cell-size))
-                           :h cell-size
-                           :w cell-size})))
+  [margin cell-size]
+  (fn [ctx cells]
+    (canvas/fill-style ctx "#000000")
+    (doseq [[row col] cells]
+      (canvas/fill-rect ctx {:x (+ margin (* col cell-size))
+                             :y (+ margin (* row cell-size))
+                             :h cell-size
+                             :w cell-size}))))
 
-(def dom-canvas (.getElementById js/document "maze-canvas"))
-(set! (.-height dom-canvas) (+ (* 2 margin) (* nrows cell-size)))
-(set! (.-width dom-canvas) (+ (* 2 margin) (* ncols cell-size)))
-
-(def mc (canvas/init dom-canvas "2d"))
-
-(canvas/add-entity mc :border (canvas/entity {:x margin :y margin :h (* nrows cell-size) :w (* ncols cell-size)}
-                                             nil
-                                             (fn [ctx border]
-                                               (-> ctx
-                                                   (canvas/stroke-style "#000000")
-                                                   (canvas/stroke-rect border)))))
-
-(canvas/add-entity mc :grid (canvas/entity (init-grid) update-grid render-grid))
+(defn init
+  [mc {:keys [nrows ncols cell-size margin] :as opts}]
+  (canvas/add-entity mc :border (canvas/entity {:x margin :y margin :h (* nrows cell-size) :w (* ncols cell-size)}
+                                               nil
+                                               (fn [ctx border]
+                                                 (-> ctx
+                                                     (canvas/stroke-style "#000000")
+                                                     (canvas/stroke-rect border)))))
+  (canvas/add-entity mc :grid (canvas/entity (init-grid nrows ncols)
+                                             (update-grid nrows ncols)
+                                             (render-grid margin cell-size))))
--- src/cljs/cam_clj/mazes/core.cljs
+++ src/cljs/cam_clj/mazes/core.cljs
@@ -3,12 +3,40 @@
   (:require [cljs.core.async :refer [chan put! <!]]
             [monet.canvas :as canvas]
             [om.core :as om]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [cam-clj.mazes.cellular-automata :as cellular-automata]))
 
-(def app-state (atom {:nrows 10 :ncols 10 :cell-size 16 :margin 4 :wall-thickness 2}))
+(def app-state (atom {:nrows 20 :ncols 20 :cell-size 16 :margin 4 :builder cellular-automata/init}))
 
-(defn placeholder
-  [{:keys [nrows ncols cell-size margin wall-thickness] :as app} owner]
+(defn parse-int
+  [v]
+  (when (and v (re-find #"^\d+$" v))
+    (js/parseInt v)))
+
+(defn update-grid-dimension
+  [app owner dim]
+  (when-let [v (-> (om/get-node owner (name dim))
+                   .-value
+                   parse-int)]
+    (om/update! app [dim] v)))
+
+(defn update-dim-button
+  [app owner {:keys [dim label]}]
+  (reify
+    om/IRender
+    (render
+     [_]
+     (dom/div nil
+              (dom/label #js {:htmlFor (name dim)} label)
+              (dom/input #js {:id (name dim)
+                              :ref (name dim)
+                              :defaultValue (app dim)
+                              :onKeyPress (fn [e] (when (== (.-keyCode e) 13)
+                                                    (update-grid-dimension app owner dim)))
+                              :onBlur (fn [_] (update-grid-dimension app owner dim))})))))
+
+(defn maze-builder
+  [{:keys [nrows ncols cell-size margin builder] :as app} owner]
   (reify
     om/IInitState
     (init-state
@@ -20,34 +48,24 @@
      (let [dom-canvas (om/get-node owner "canvas")
            mc         (canvas/init dom-canvas "2d")
            ch         (om/get-state owner [:ch])]
-       (canvas/add-entity mc :border (canvas/entity
-                                      {:x margin :y margin :h (* nrows cell-size) :w (* ncols cell-size)}
-                                      nil
-                                      (fn [ctx border]
-                                        (-> ctx
-                                            (canvas/stroke-style "#000000")
-                                            (canvas/stroke-rect border)))))
-       (canvas/add-entity mc :walker (canvas/entity
-                                      {:row 0 :col 0}
-                                      (fn [{:keys [row col]}]
-                                        (let [[delta-row delta-col] (rand-nth [[0 1] [0 -1] [1 0] [-1 0]])]
-                                          {:row (mod (+ row delta-row) nrows)
-                                           :col (mod (+ col delta-col) ncols)}))
-                                      (fn [ctx {:keys [row col]}]
-                                        (-> ctx
-                                            (canvas/fill-style "red")
-                                            (canvas/fill-rect {:x (+ margin (* col cell-size))
-                                                               :y (+ margin (* row cell-size))
-                                                               :h cell-size
-                                                               :w cell-size})))))
+       (om/set-state! owner :monet-canvas mc)
+       (builder mc (select-keys app [:nrows :ncols :cell-size :margin]))
        (go (loop []
              (when-let [action (<! ch)]
                (case action
                  :stop (canvas/stop mc)
                  :restart (canvas/restart mc)
                  :stop-updating (canvas/stop-updating mc)
-                 :start-updating (canvas/start-updating mc))
+                 :start-updating (canvas/start-updating mc)
+                 :refresh (do
+                            (canvas/clear! mc)
+                            ((:builder @app) mc (select-keys @app [:nrows :ncols :cell-size :margin]))))
                (recur))))))
+    om/IDidUpdate
+    (did-update
+     [_ prev-props prev-state]
+     (let [mc (om/get-state owner [:monet-canvas])]
+       ((:builder app) mc (select-keys app [:nrows :ncols :cell-size :margin]))))
     om/IRenderState
     (render-state
      [_ {:keys [ch]}]
@@ -55,12 +73,16 @@
               (dom/canvas #js {:height (+ (* 2 margin) (* nrows cell-size))
                                :width  (+ (* 2 margin) (* ncols cell-size))
                                :ref "canvas"})
+              (dom/hr nil)
+              (om/build update-dim-button app {:opts {:dim :nrows :label "Rows"}})
+              (om/build update-dim-button app {:opts {:dim :ncols :label "Columns"}})
               (dom/button #js {:onClick (fn [_] (put! ch :stop))} "Stop")
               (dom/button #js {:onClick (fn [_] (put! ch :restart))} "Restart")
               (dom/button #js {:onClick (fn [_] (put! ch :stop-updating))} "Stop Updating")
-              (dom/button #js {:onClick (fn [_] (put! ch :start-updating))} "Start Updating")))))
+              (dom/button #js {:onClick (fn [_] (put! ch :start-updating))} "Start Updating")
+              (dom/button #js {:onClick (fn [_] (put! ch :refresh))} "Refresh")))))
 
 (om/root
- placeholder
+ maze-builder
  app-state
  {:target (. js/document (getElementById "app"))})
